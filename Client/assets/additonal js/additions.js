cfe181adc04af247e930ee5fe062a93d2b5aed68
barakdo <barakdoron3@gmail.com>
2024-08-27 03:20:58 +0300

    Updates

--- Client/assets/additonal js/additions.js
+++ Client/assets/additonal js/additions.js
@@ -22,22 +22,24 @@ $(document).ready(function () {
             document.getElementById("titleHeader2").innerText = "The Site's Special Features!";    
         break;
     }
-    getBooksCat();   
+    // getBooksCat();
+    displayBooks();  
+    console.log(getFeatures()); 
 });
 
-function getBooksCat() {
-    let port = portGlobal;
-    let api = apiGlobal + port + "/api/Categories/allBooksAllCategory";
-    ajaxCall("GET", api, "", getBooksCatSCB, getBooksCatECB);
-}
+// function getBooksCat() {
+//     let port = portGlobal;
+//     let api = apiGlobal + port + "/api/Categories/allBooksAllCategory";
+//     ajaxCall("GET", api, "", getBooksCatSCB, getBooksCatECB);
+// }
 
-function getBooksCatSCB(booksCat) {
-    GetBooks(booksCat);
-}
+// function getBooksCatSCB(booksCat) {
+//     GetBooks(booksCat);
+// }
 
-function getBooksCatECB(err) {
-    console.log(err);
-}
+// function getBooksCatECB(err) {
+//     console.log(err);
+// }
 
 
 
@@ -53,118 +55,107 @@ function muteChange(){
     }
 }
 
-function ajaxCall(method, api, data, successCB, errorCB) {
-    $.ajax({
-        type: method,
-        url: api,
-        data: data,
-        cache: false,
-        contentType: "application/json",
-        dataType: "json",
-        success: successCB,
-        error: errorCB
-    });
-}
+// function ajaxCall(method, api, data, successCB, errorCB) {
+//     $.ajax({
+//         type: method,
+//         url: api,
+//         data: data,
+//         cache: false,
+//         contentType: "application/json",
+//         dataType: "json",
+//         success: successCB,
+//         error: errorCB
+//     });
+// }
 
-function GetBooks(booksCat) {
-    let port = portGlobal;
-    let api = apiGlobal + port;
-    api+= '/api/Authors/allAuthorsAllBooks';
-    ajaxCall("GET", api, "", function (data) { GetGetBooksSCB(data, booksCat) }, GetGetBooksECB); 
-}
+// function GetBooks(booksCat) {
+//     let port = portGlobal;
+//     let api = apiGlobal + port;
+//     api+= '/api/Authors/allAuthorsAllBooks';
+//     ajaxCall("GET", api, "", function (data) { GetGetBooksSCB(data, booksCat) }, GetGetBooksECB); 
+// }
 
-function GetGetBooksSCB(books, booksCat) {
-    console.log(books);
-    displayBooks(books, booksCat);
-}
+// function GetGetBooksSCB(books, booksCat) {
+//     console.log(books);
+//     displayBooks(books, booksCat);
+// }
 
-function GetGetBooksECB(err) {
-    console.log(err);
-}
+// function GetGetBooksECB(err) {
+//     console.log(err);
+// }
 
 
-function displayBooks(books, booksCat) {
+function displayBooks() {
+    let features = getFeatures();
+    console.log(features);
     var str = ""; 
-    let count  = 0;
-    let authorCount = 0;
-    let lst = new Array();
-    let flag = false;
-    for(let i = 0; i < books.length; i++){
-        if(!lst.includes(books[i]['authorId'])){
+    for(let i = 0; i < features.length; i++){
+        
             str += '<div class="shop-list-items" style="background-color: #012E4A; color: white;padding: 20px 20px 20px 20px; font-weight: 700">';
-            str += 'All Books Written By: ';
-            str += '<a style="color:white" href="author.html?authorId=' + books[i]["authorId"] + '">';
-            str+= books[i]['authorName'];
-            str += '</a>';
-            str+= '</div>';
-            lst.push(books[i]['authorId']);
-            authorCount++;
-            flag = true;
-        }
-            dict[count] = books[i]['bookId'];      
+            str += 'Feature #'+ Number(i+1) +' - ' + features[i]["title"];
+            str+= '</div>';    
             str+= '<div class="shop-list-items"><div class="shop-list-thumb"><img style="height: 300px; cursor: pointer" id="img' + i + '" src="';
-            str+= books[i]['coverImgUrl']; //image
-            if(books[i]['coverImgUrl'] == "")
+            str+= features[i]['img']; //image
+            if(features[i]['img'] == "")
                 {
                     str+= 'assets/img/noCover.png';
                 }
-            str+= '" alt="img"></div> <div class="shop-list-content" style="padding: 30px 0px 30px 0px;"><h3><a id="title' + i + '">';
-            str+= books[i]['title']; //title
-            str+= '</a></h3><h5>';
-            str+= books[i]['price'] + "$</h5><br></br>"; //price
-            str+= '<p class= "author' + books[i]['bookId'] + '"></p>'; //author
-            // str+= '<div class="star">';
-            //str+= '<i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-regular fa-star"></i></div>'; 
+            str+= '" alt="img"></div> <div class="shop-list-content" style="padding: 30px 30px 30px 30px;"><h3><a id="title' + i + '">';
+            str+= features[i]['title']; //title
+            str+= '</a></h3>';
+            // str+='<h5>';
+            // str+= features[i]['title'] + "</h5>"
+            // str+="<br></br>"; //price
+            //str+= '<p class= "author' + features[i]['id'] + '"></p>'; //author
             str+= '<p id="desc';
-            str+=  books[i]["bookId"]+ '">';
-            str+= books[i]['description']; //description 
+            str+=  features[i]["id"]+ '">';
+            str+= features[i]['desc']; //description 
             str+= '</p> <div class="shop-btn">';
-            //str+= '<a href="shop-details.html" class="theme-btn"><i'; 
-            //str+= 'class="fa-solid fa-basket-shopping"></i> Add To Cart</a>';
             str+= '<ul class="shop-icon d-flex justify-content-center align-items-center">'; 
             str+= '<li onclick="' + 'Read(';
-            str+= '&#x0022 ' + books[i]["description"].replace(new RegExp('"', 'g'), '') + '&#x0022';
+            str+= '&#x0022 ' + features[i]["desc"].replace(new RegExp('"', 'g'), '') + '&#x0022';
             str+= ')" id="speak';
-            str += books[i]["bookId"];
+            str += features[i]["id"];
             str+= '"><i class="fa-solid fa-volume-high"></i></li>';
-            //str+= '<li><a href="shop-cart.html"><i class="far fa-heart"></i></a></li>';
-            str+= '<li id="likeBtn'+ count+'"><a class="icon-2"><i id="likeSym'+ count+'" class="far fa-heart"></i></a></li>';
-            //str+= '<li> <a href="shop-cart.html"><img class="icon" src="assets/img/icon/shuffle.svg" alt="svg-icon"> </a> </li>'; 
-            str+= '<li id="eyeBtn'+ count+'"> <a class="icon-2"> <i id="eyeSym'+ count+'" class="far fa-eye"></i></a> </li> </ul>';
-            //str+= '<li> <a href="shop-details.html"><i class="far fa-eye"></i></a> </li> </ul>';
             str+= '</div> </div> </div>';
-            count++;
-        // if(flag){
-        //     str+= '';
-        //     flag = false;
-        // }   
     }
     document.getElementById("booksList").innerHTML = str;
-    for(let i = 0; i < books.length; i++){
-        for(let j = 0; j < booksCat.length; j++){
-            if(books[i]["bookId"] == booksCat[j]["bookId"]){
-                $("#img" + i).click(function(){
-                    const params = $.param({
-                        bookId: booksCat[j]["bookId"],
-                        categoryName: booksCat[j]["categoryName"]
-                        
-                    });
-                    window.location.href = 'shop-details.html?' + params;
-                });
-                $("#title" + i).click(function(){
-                    const params = $.param({
-                        bookId: booksCat[j]["bookId"],
-                        categoryName: booksCat[j]["categoryName"]
-                        
-                    });
-                    window.location.href = 'shop-details.html?' + params;
-                });
-                break;
-            }
-        }
+    document.getElementById("resultsNum").innerText = "Showing " + features.length + " Exciting Features!";
+}
+function Read(info){
+    if(audio != null){
+        audio.volume = 0.2;
     }
-            document.getElementById("resultsNum").innerText = "Showing 4 Exciting Features!";
+    responsiveVoice.speak(info, "UK English Male", {rate: 0.9});
 }
 
-
+function getFeatures(){
+    const features = [
+         {
+            "id": 0,
+            "title": "Text-to-Speech Feature with ResponsiveVoice",
+            "desc": "This feature allows users to have any text on the website read aloud to them using the ResponsiveVoice text-to-speech engine. Users can click a designated button to hear the main content or select specific text on the page to be read aloud. This provides an accessible and convenient experience for users who prefer auditory learning or need assistance with reading.",
+            "img": "assets/img/features/voice.png"
+        },
+         {
+            "id": 1,
+            "title": "Genre-Specific Background Music",
+            "desc": "Each book category and individual book page on the site is accompanied by music specifically curated to match the genre. With 70 unique music tracks available, every page immerses users in the atmosphere of the genre, enhancing their browsing experience and making it more enjoyable.",
+            "img": "assets/img/features/music.png"
+        },
+         {
+            "id": 2,
+            "title": "'Read A Little' Preview with Google Books",
+            "desc": "For selected books, users can click the 'Read A Little' button on each book's page to open a new window where they can preview a portion of the book. This feature is powered by Google Books embedding, allowing users to sample the book before deciding to trade or purchase it.",
+            "img": "assets/img/features/preview.png"
+        },
+         {
+            "id": 3,
+            "title": "Advanced Book and Author Search",
+            "desc": "The advanced search feature allows users to search by book title, description, publisher, publication year, or author. Search results dynamically lead to either a book list page or an author list page, depending on the query. This flexible search functionality ensures users can quickly find books or discover other works by their favorite authors",
+            "img": "assets/img/features/search.png"
+        }        
+    ];
+    return features;
+}
 
